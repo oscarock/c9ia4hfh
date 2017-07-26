cae067a69f002a8274042be4bc20acde2c615b4b
Your Name <you@example.com>
2017-07-26 11:51:53 -0500

    solucion bug reto 5 rails plataforma

--- app/models/room.rb
+++ app/models/room.rb
@@ -17,4 +17,5 @@ class Room < ActiveRecord::Base
     validates :description, presence: true, length: { maximum: 400 }
     validates :beds, presence: true, :numericality => {:only_integer => true}
     validates :guests, presence: true, :numericality => {:only_integer => true}
+    validates :image_url, presence: true
 end
