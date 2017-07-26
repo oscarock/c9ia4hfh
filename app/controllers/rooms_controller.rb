8f0e8f939506bcc92bbc1cde5f9af64ee0b3d0c0
Your Name <you@example.com>
2017-07-26 15:08:06 -0500

    reto 7 rails plataforma ok :)

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -17,6 +17,26 @@ class RoomsController < ApplicationController
     end
   end
 
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+        redirect_to '/rooms'
+    else
+        render 'edit'
+    end
+  end
+
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+
+    redirect_to '/rooms'
+  end
+
   protected
     def room_params
       params.require(:room).permit(:title, :description, :beds, :guests, :image_url, :price_per_night)
